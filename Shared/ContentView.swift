63e70ff501374a7fdf14604b8629d38a3f11045a
Grant <grant.ales@yahoo.com>
2021-10-07 22:08:58 -0600

    Changes to the UI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,83 +6,46 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink(destination: Text("Item at \(item.timestamp!, formatter: itemFormatter)")) {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
+        
+      ZStack {
+          Image("app-background").ignoresSafeArea()
+        
+          VStack{
+              HStack {
+                    Button(action: {}, label: {
+                        Image("Happy Emoji").resizable()
+                                            .frame(width: 100, height:100)
+                    })
+                  
+                    Button(action: {}, label: {
+                        Image("Average Emoji").resizable()
+                                              .frame(width: 100, height:100)
+                    })
+          }
+              
+              HStack {
+                  Button(action: {}, label: {
+                        Image("Sad Emoji").resizable()
+                                          .frame(width: 100, height:100)
+                  })
+                
+                  Button(action: {}, label: {
+                      Image("Mad Emoji").resizable()
+                                          .frame(width: 100, height:100)
+                  })
+              }
+          Text("Select the emoji to input your mood.")
+                  .padding(9.0)
+              .font(.headline)
+      }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+  }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    static var previews: some View{
+        ContentView()
     }
 }
