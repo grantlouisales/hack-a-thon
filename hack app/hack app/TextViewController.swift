83dbbad4d2c67aafd6cdda211f565abee6750608
Devan Hoyt <38635082+devanhoyt@users.noreply.github.com>
2021-10-09 12:27:05 -0600

    Add files via upload

---
+++ hack app/hack app/TextViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TextViewController.swift
+//  hack app
+//
+//  Created by Devan Hoyt on 10/9/21.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet var dailynote: UITextField!
+    
+    @IBAction func save_button(_ sender: Any) {
+    }
+    
+}
---
+++ hack app/hack app/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  hack app
+//
+//  Created by Devan Hoyt on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    @IBAction func happyTapped(_ sender: Any) {
+        
+        
+        //present(SecondViewController(), animated: true)
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "text_vc") as? TextViewController else {
+            return
+        }
+        present(vc, animated: true)
+        
+    }
+    
+    
+    @IBAction func neutralTapped(_ sender: Any) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "text_vc") as? TextViewController else {
+            return
+        }
+        present(vc, animated: true)
+        
+        //present(SecondViewController(), animated: true)
+    }
+    
+    
+    
+    @IBAction func angryTapped(_ sender: Any) {
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "text_vc") as? TextViewController else {
+            return
+        }
+        present(vc, animated: true)
+        //present(SecondViewController(), animated: true)
+        
+    }
+    
+    
+    
+    @IBAction func sadTapped(_ sender: Any) {
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "text_vc") as? TextViewController else {
+            return
+        }
+        present(vc, animated: true)
+        //present(SecondViewController(), animated: true)
+        
+    }
+    
+    
+    
+}
+
+/*class SecondViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+*/
+
+    
+   
+    
+    
+    
+
+
