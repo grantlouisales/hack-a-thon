80b4b93b34d2baaf6845863c99c99e30927ca0f6
Grant <grant.ales@yahoo.com>
2021-10-09 16:58:42 -0600

    finished all of the typos

--- hack app/hack app/ViewController.swift
+++ hack app/hack app/ViewController.swift
@@ -9,22 +9,22 @@ import UIKit
 
 public var state = ""
 public var response = ""
-public let happyResponses = ["Be proud of yourself for how hard youre working", "Great job! Keep it up!",
+public let happyResponses = ["Be proud of yourself for how hard you're working", "Great job! Keep it up!",
                              "Even if you are on the right track, you will get run over if you just sit there so keep going!",
                              "If I sang the praises of your hard work, I would lose my voice quickly!"]
 
 public let neutralResponses = ["Be happy in the moment, thats enough. Each moment is all we need, not more.",
-                               "When it rains look for rainbows, when its dark look for stars.",
+                               "When it rains, look for rainbows, when it's dark look for stars.",
                                "Do one thing every day that makes you happy.",
                                "Take time to make yourself happy, do more of what makes you happy.",
                                "Do what makes you happy. When it doesn't make you happy anymore, make a change without fear."]
 
-public let angryResponses = ["Tough times never last, bout tough people do.", "Anger makes you smaller, while forgiveness  forces you to grow beyond what you are",
+public let angryResponses = ["Tough times never last, but tough people do.", "Anger makes you smaller, while forgiveness  forces you to grow beyond what you are",
                              "There are two things a person should never be angry at, what they can help, and what they cannot.",
-                             "When angry count ten before you speak; if very angry, a hundred.", "Calm is the best revenge",
+                             "When angry, count to ten before you speak; if very angry, count to a hundred.", "Calm is the best revenge",
                              "If you are patient in one moment of anger, you will escape a hundred days of sorrow."]
 
-public let sadResponses = ["Every day is not the same, you need to wait and be patient fo the good things to happen",
+public let sadResponses = ["Every day is not the same, you need to wait and be patient for the good things to happen",
                            "Be strong now because things will get better. It might be stormy now, but it can't rain forever",
                            "Do not lose hope, believe that there are a thousand beautiful things waiting for you.",
                            "Sunshine comes to all who feel rain, you matter.",
