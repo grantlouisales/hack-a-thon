915fcf9d41d9372351696e28801f89bf71aa0b8e
Devan Hoyt <38635082+devanhoyt@users.noreply.github.com>
2021-10-09 18:01:41 -0600

    Update TextViewController.swift

--- hack app/hack app/TextViewController.swift
+++ hack app/hack app/TextViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SwiftUI
+
 
 class TextViewController: UIViewController {
 
@@ -16,6 +18,18 @@ class TextViewController: UIViewController {
     }
     
 
+    @IBAction func quote_butt(_ sender: Any) {
+        let alertController = UIAlertController(title: "Quote", message: quote, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default) {
+            (action: UIAlertAction!) in
+            // Code in this block will trigger when OK button tapped.
+            print("Ok button tapped");
+        }
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+ 
     @IBOutlet var dailynote: UITextField!
     
     @IBAction func save_button(_ sender: Any) {
